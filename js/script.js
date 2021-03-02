ac070a1462e309aeff948d0f09492190106f66aa
Alfredo Ficocelli <alfredo.ficocelli@gmail.com>
2021-03-02 16:26:32 +0100

    esercizio completato

--- js/script.js
+++ js/script.js
@@ -2,25 +2,23 @@
 ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.
 Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.*/
 
-
-
   for ( i = 0 ; i < 100; i++ ) {
 
-    if ( insiemeNumeri[i] %5 == 0 && insiemeNumeri[i] %3 == 0 ) {
+    if ( i %5 == 0 && i %3 == 0 ) {
 
       console.log( "FizzBuzz" );
 
-    } else if ( insiemeNumeri[i] %5 == 0 ){
+    } else if ( i %5 == 0 ){
 
       console.log( "Buzz" );
 
-    } else if ( insiemeNumeri[i] %3 == 0 ) {
+    } else if ( i %3 == 0 ) {
 
       console.log( "Fizz" );
 
     } else {
 
-      console.log( insiemeNumeri[i] );
+      console.log( i );
 
     }
 

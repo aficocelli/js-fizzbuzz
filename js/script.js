1522efb1110ff6cfb3d85c74ef95f6f14210bb35
Alfredo Ficocelli <alfredo.ficocelli@gmail.com>
2021-03-02 14:25:48 +0100

    add. scheme

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
+/*Scrivi programma che stampi i numeri da 1 a 100,
+ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.
+Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.*/
 
+
+// 1 ciclo for per stampare numero da 1 a 100
+
+
+
+
+
+
+// 2 se numero è multiplo di 3 Fizz
+
+
+
+
+
+
+// 3 se numero è multiplo di 5 Buzz
+
+
+
+
+// 4 se numero è multiplo sia di 3 che di 5 fizzbuzz

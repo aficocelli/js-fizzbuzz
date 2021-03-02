b2a1e91a892e6f6274ff92628ec8b9986428c69e
Alfredo Ficocelli <alfredo.ficocelli@gmail.com>
2021-03-02 14:53:48 +0100

    add. ciclo for  per numeri multipli

--- js/script.js
+++ js/script.js
@@ -5,13 +5,42 @@ Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.*/
 
 // 1 ciclo for per stampare numero da 1 a 100
 
+  // definisco variabile con valore 0
 
+  var numero = 0;
 
+  var insiemeNumeri = [];
 
+  for (  var i = 0; i < 100; i++ ) {
 
+    numero = numero + 1;
 
-// 2 se numero è multiplo di 3 Fizz
+    // creo array con tutti i 100 numeri
+    insiemeNumeri.push(numero);
 
+  }
+
+  // 2 se numero è multiplo di 3 Fizz
+
+  for( i = 0; i < 100; i++ ) {
+
+    if ( (insiemeNumeri[i] %5 == 0) && (insiemeNumeri[i] %3 == 0) ) {
+
+      insiemeNumeri[i] = "FizzBuzz";
+
+    } else if ( insiemeNumeri[i] %5 == 0 ){
+
+      insiemeNumeri[i] = "Buzz";
+
+    } else if ( insiemeNumeri[i] %3 == 0 ) {
+
+      insiemeNumeri[i] = "Fizz";
+
+    }
+
+  }
+
+  console.log(insiemeNumeri);
 
 
 
